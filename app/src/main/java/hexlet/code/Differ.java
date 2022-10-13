4d16e6cc9ad68d2d2af017233f826601230e45c6
Hanna Mudrak <annmudrak@gmail.com>
2022-10-13 13:29:18 -0500

    add module

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,2 +1,9 @@
-package hexlet.code;public class Differ {
+package hexlet.code;
+
+import java.util.Map;
+
+public class Differ {
+    public static String generate(Map<String, Object> map1, Map<String, Object> map2) {
+        return " ";
+    }
 }
