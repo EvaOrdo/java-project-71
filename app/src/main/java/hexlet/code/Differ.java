2aa5401e3667984d4fffba9430181359a6bff334
Hanna Mudrak <annmudrak@gmail.com>
2022-10-13 17:00:04 -0500

    add generate method

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,9 +1,43 @@
 package hexlet.code;
 
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Differ {
-    public static String generate(Map<String, Object> map1, Map<String, Object> map2) {
-        return " ";
+    public static String generate(String filepath1, String filepath2) throws Exception {
+        Map<String, Object> map1 = Parser.parseJSON(filepath1);
+        Map<String, Object> map2 = Parser.parseJSON(filepath2);
+        System.out.println("map1: " + map1);
+        System.out.println("map2: " + map2);
+
+        Set<String> keys = new TreeSet<>();
+        keys.addAll(map1.keySet());
+        keys.addAll(map2.keySet());
+
+        System.out.println("keys: " + keys);
+
+        String result = "{\n";
+
+        for (String key : keys) {
+            Object value1 = map1.get(key) != null ? map1.get(key) : "null";
+            Object value2 = map2.get(key) != null ? map2.get(key) : "null";
+            System.out.println("1: " + value1);
+            System.out.println("2: " + value2);
+            if (!map1.containsKey(key)) {
+                result += "  + " + key + ": " + value2 + "\n";
+            } else if (!map2.containsKey(key)) {
+                result += "  - " + key + ": " + value1 + "\n";
+            } else {
+                if (value1.equals(value2)) {
+                    result += "    " + key + ": " + value1 + "\n";
+                } else {
+                    result += "  - " + key + ": " + value1 + "\n"
+                            + "  + " + key + ": " + value2 + "\n";
+                }
+            }
+        }
+        System.out.println(result + "}");
+        return result + "}";
     }
 }
