f2c6e20442525e0e87b02156991c3b581a154c50
Hanna Mudrak <annmudrak@gmail.com>
2022-10-23 18:13:26 -0500

    improve null exception

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -20,27 +20,44 @@ public class Differ {
         Set<Map> diff = new LinkedHashSet<>();
 
         for (var key : commonKeys) {
-            Object value1 = map1.get(key) != null ? map1.get(key) : "null";
-            Object value2 = map2.get(key) != null ? map2.get(key) : "null";
             Map<String, Object> map = new TreeMap<>();
+
             if (!map1.containsKey(key)) {
                 map.put("status", "added");
                 map.put("key", key);
-                map.put("value2", value2);
-            } else if (!map2.containsKey(key)) {
+                map.put("value2", map2.get(key));
+            }
+
+            if (!map2.containsKey(key)) {
                 map.put("status", "removed");
                 map.put("key", key);
-                map.put("value1", value1);
-            } else {
-                if (value1.equals(value2)) {
-                    map.put("status", "unchanged");
-                    map.put("key", key);
-                    map.put("value1", value1);
-                } else {
-                    map.put("status", "changed");
-                    map.put("key", key);
-                    map.put("value1", value1);
-                    map.put("value2", value2);
+                map.put("value1", map1.get(key));
+            }
+            if (map1.containsKey(key) && map2.containsKey(key)) {
+                Object value1 = map1.get(key);
+                Object value2 = map2.get(key);
+                if (value1 != null) {
+                    if (value1.equals(value2)) {
+                        map.put("status", "unchanged");
+                        map.put("key", key);
+                        map.put("value1", value1);
+                    } else {
+                        map.put("status", "changed");
+                        map.put("key", key);
+                        map.put("value1", value1);
+                        map.put("value2", value2);
+                    }
+                } else if (value1 == null) {
+                    if (value2 != null) {
+                        map.put("status", "changed");
+                        map.put("key", key);
+                        map.put("value1", null);
+                        map.put("value2", value2);
+                    } else {
+                        map.put("status", "unchanged");
+                        map.put("key", key);
+                        map.put("value1", null);
+                    }
                 }
             }
             diff.add(map);
