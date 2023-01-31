93204e6de812afb476cc11ea2fed5e95f4a80f99
Hanna Mudrak <annmudrak@gmail.com>
2023-01-30 19:12:16 -0600

    improve render method

--- app/src/main/java/hexlet/code/formatters/Json.java
+++ app/src/main/java/hexlet/code/formatters/Json.java
@@ -7,12 +7,7 @@ import java.util.Set;
 
 public class Json {
     public static String render(Set<Map> diff) throws Exception {
-        String result = new String();
         ObjectMapper mapper = new ObjectMapper();
-        for (Map map : diff) {
-            String mapToJsonString = mapper.writeValueAsString(map);
-            result += mapToJsonString;
-        }
-        return result;
+        return mapper.writeValueAsString(diff);
     }
 }
