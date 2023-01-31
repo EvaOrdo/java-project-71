b911470e6084d928dc2fe74f3bcc173714ce77d4
Hanna Mudrak <annmudrak@gmail.com>
2023-01-30 19:17:21 -0600

    move difference building logic to DiffBuilder class

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,64 +1,40 @@
 package hexlet.code;
 
-import java.util.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
+import static hexlet.code.DiffBuilder.buildDifference;
 
 public class Differ {
+    private static String getExtension(String filename) {
+        if (!filename.contains(".")) {
+            return "";
+        }
+        return filename.substring(filename.lastIndexOf(".") + 1).toLowerCase();
+    }
+
+    private static String getContent(String filepath) throws IOException {
+        Path path = Paths.get(filepath).toAbsolutePath();
+        return Files.readString(path);
+    }
     public static String generate(String filepath1, String filepath2, String formatName)
             throws Exception {
-        Map<String, Object> map1 = Parser.parseString(filepath1);
-        Map<String, Object> map2 = Parser.parseString(filepath2);
+
+        Map<String, Object> map1 = Parser.parseString(getContent(filepath1), getExtension(filepath1));
+        Map<String, Object> map2 = Parser.parseString(getContent(filepath2), getExtension(filepath2));
 
         Set<String> commonKeys = new TreeSet<>();
 
         commonKeys.addAll(map1.keySet());
         commonKeys.addAll(map2.keySet());
 
-        Set<Map> diff = new LinkedHashSet<>();
+        Set<Map> diff = buildDifference(commonKeys, map1, map2);
 
-        for (var key : commonKeys) {
-            Map<String, Object> map = new TreeMap<>();
-
-            if (!map1.containsKey(key)) {
-                map.put("status", "added");
-                map.put("key", key);
-                map.put("value2", map2.get(key));
-            }
-
-            if (!map2.containsKey(key)) {
-                map.put("status", "removed");
-                map.put("key", key);
-                map.put("value1", map1.get(key));
-            }
-            if (map1.containsKey(key) && map2.containsKey(key)) {
-                Object value1 = map1.get(key);
-                Object value2 = map2.get(key);
-                if (value1 != null) {
-                    if (value1.equals(value2)) {
-                        map.put("status", "unchanged");
-                        map.put("key", key);
-                        map.put("value1", value1);
-                    } else {
-                        map.put("status", "changed");
-                        map.put("key", key);
-                        map.put("value1", value1);
-                        map.put("value2", value2);
-                    }
-                } else if (value1 == null) {
-                    if (value2 != null) {
-                        map.put("status", "changed");
-                        map.put("key", key);
-                        map.put("value1", null);
-                        map.put("value2", value2);
-                    } else {
-                        map.put("status", "unchanged");
-                        map.put("key", key);
-                        map.put("value1", null);
-                    }
-                }
-            }
-            diff.add(map);
-        }
-        System.out.println(Formatter.format(diff, formatName));
         return Formatter.format(diff, formatName);
     }
 
